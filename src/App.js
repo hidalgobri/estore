ff9e16447d0a4838dfaf320d8bf37209b6478043
Valeria Hidalgo <valeriahidalgo50@gmail.com>
2022-09-23 16:16:49 -0500

    hasta contador carrito

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@
 import data from "./data.json"
 import ItemsLista from "./components/ItemsLista"
 import { Routes, Route } from "react-router-dom"
+import { useSelector } from "react-redux"
+
+const CarritoCounter = () =>{
+  const carrito = useSelector(state => state.carrito)
+
+  return <h1>Carrito contador {carrito.length}</h1>
+  
+
+
+}
 
 
 function App() {
@@ -9,7 +19,7 @@ function App() {
   return (
     <>
       <h1>Hola</h1>
-
+      <CarritoCounter></CarritoCounter>
       <Routes>
         <Route path="/" element={<ItemsLista itemsLista={data.items}/>}/>
       </Routes>
--- src/components/ItemsLista.js
+++ src/components/ItemsLista.js
@@ -1,15 +1,16 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import { ItemContainer, ItemCard, ComprarButton } from "./styles/Item.styled";
 
 const ItemsLista = () => {
-    const items = useSelector(state => state.item)  
-
+    const items = useSelector(state => state.item) 
+    const dispatch = useDispatch() 
+  
     const renderedListItems = items.map(item => {
-      return <ItemCard>
+      return <ItemCard key={item.id}>
         <img src={require("../assets/"+item.img)}></img>
         <h3>{item.nombre}</h3>
         <p>$ {item.precio_kg}</p>
-        <ComprarButton>Comprar</ComprarButton>
+        <ComprarButton onClick={() => {dispatch({type: "carrito/anadirItem", payload: item.id} )} }>Comprar</ComprarButton>
       </ItemCard>
     })
 
