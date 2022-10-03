b4d682464ae6db25cebfce8988e7f88320e6bde9
Valeria Hidalgo <valeriahidalgo50@gmail.com>
2022-10-03 12:05:35 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import GlobalStyles from "./components/styles/GlobalSyles.styled";
 import App from "./App";
 import store from "./store";
@@ -10,10 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <GlobalStyles />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
